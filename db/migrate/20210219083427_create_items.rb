4048093701d51acdbbbe249707b9f44a6be89369
Takeshi <k12te101@icloud.com>
2021-02-19 23:37:59 +0900

    itemテーブルの修正

--- db/migrate/20210219083427_create_items.rb
+++ db/migrate/20210219083427_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.sting     :name, null: false
+      t.string     :name, null: false
       t.text      :description, null: false
       t.integer   :price, null: false
       t.integer   :status_id, null: false
@@ -9,7 +9,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer   :delivery_fee_id, null: false
       t.integer   :category_id, null: false
       t.integer   :prefecture_id, null: false
-      t.reference :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
