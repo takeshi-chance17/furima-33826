c71ce07b5fb013a53e20b016fd158606d405f828
Takeshi <k12te101@icloud.com>
2021-02-19 17:44:17 +0900

    item modelの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,42 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_134341) do
-
-  create_table "adresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "phone_number", null: false
-    t.string "postal_code", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", null: false
-    t.string "house_number", null: false
-    t.string "building_name"
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_adresses_on_item_id"
-    t.index ["user_id"], name: "index_adresses_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2021_02_19_083427) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.text "description", null: false
-    t.text "condition", null: false
-    t.integer "price", null: false
-    t.boolean "status", null: false
-    t.boolean "delivery_fee", null: false
-    t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_user_items_on_item_id"
-    t.index ["user_id"], name: "index_user_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -66,8 +35,4 @@ ActiveRecord::Schema.define(version: 2021_02_16_134341) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "adresses", "items"
-  add_foreign_key "adresses", "users"
-  add_foreign_key "user_items", "items"
-  add_foreign_key "user_items", "users"
 end
