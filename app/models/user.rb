4497d28b1e1021adac3318c122b73b297d36c254
Takeshi <k12te101@icloud.com>
2021-02-22 21:14:53 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :buy_histories
+
 
   with_options presence: true do
     validates :password,
