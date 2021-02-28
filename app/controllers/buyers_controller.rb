ebccffa984d284a6f1824b7eeef94e784559c17b
Takeshi <k12te101@icloud.com>
2021-02-28 15:55:45 +0900

    URLの直打ち対策

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,13 +1,13 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :sign_in_confirmation, only: [:index, :create]
 
   def index
     @buyer_item = BuyerItem.new
-    @item = Item.find(params[:format])
+    sale_confirmation
   end
 
   def create
-    @item = Item.find(params[:format])
     @buyer_item = BuyerItem.new(buyer_params)
     if @buyer_item.valid?
       Payjp.api_key = ENV['PAYJP_SECRET_KEY']
@@ -29,5 +29,24 @@ class BuyersController < ApplicationController
     params.require(:buyer_item).permit(:phone_number, :postal_code,
                                        :prefecture_id, :city, :house_number,
                                        :building_name).merge(token: params[:token])
+  end
+
+  def sale_confirmation
+    h = BuyHistory.all
+    h.length.times do |i|
+      if @item.id == h[i].item_id
+        redirect_to root_path
+      end
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:format])
+  end
+
+  def sign_in_confirmation
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
