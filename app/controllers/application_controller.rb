aa9af3c042132aa6ca04accb21e5c470e30f65b0
Takeshi <k12te101@icloud.com>
2021-03-01 11:19:29 +0900

    Basic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_parmitted_parameters, if: :devise_controller?
 
   private
@@ -6,5 +7,10 @@ class ApplicationController < ActionController::Base
   def configure_parmitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :first_name, :last_name, :first_name_read, :last_name_read, :birth_day])
+  end
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
